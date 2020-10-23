59aad3a754dcd4d47b8179bda3345a6728109aeb
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2020-10-23 09:18:54 -0400

    Creating Fizzbuzz with IF statements

--- Fizzbuzz/Fizzbuzz/Program.cs
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -6,7 +6,24 @@ namespace Fizzbuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 100; i++)
+            {
+                if(i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
